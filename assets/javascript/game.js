6a29b662974b93cf2293cf5f534a6d3a9f35da1d
Tim Tucker <tntucker0015@outlook.com>
2018-03-15 15:01:08 -0400

    adding wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,8 @@
+// need to fix the following:
+    // start button needs to reset yourscore at the begining (adding a render function)
+    // number generation needs to be random for images
+    // game needs to stop afte you win and you lose.
+    // fix overall layout and look
 $(document).ready(function() {
   var options = [10, 5, 2, 1];
 //   var crystals = ["earth", "air", "fire", "water"];
@@ -9,6 +14,7 @@ $(document).ready(function() {
       fire: [],
       water: [],
       yourScore: 0,
+      targetScore: 0,
       wins: 0,
       losses: 0,
       random: 0,
@@ -17,6 +23,7 @@ $(document).ready(function() {
 // // resets your score to 0, invokes target number and sets crystals to random number
     function startGame() {
        targetNumber();  
+       record();
     };
 
     // creates a loop to iterate through the array of options - assigns img, class and value
@@ -26,35 +33,38 @@ $(document).ready(function() {
         imageCrystal.attr("src", crystalSrc[i] );
         imageCrystal.attr("data-crystalValue", options[i]);
         crystals.append(imageCrystal);
+        
     }
 
   
     // places and prints a random number in target value variable 
     function targetNumber () {
-        var targetScore = 0;
-        targetScore = Math.floor(Math.random() * 100) + 25;
-        $("#targetScore").text(targetScore);
+        game.targetScore = Math.floor(Math.random() * 100) + 25;      
+        $("#targetScore").text(game.targetScore);
     };
+    function record () {
+        console.log(game.yourScore);
+        console.log(game.targetScore);
+    }
     // captures click of image
     crystals.on("click", ".crystal-image", function() {
         var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-        game.yourScore += crystalValue;
-        $("#yourScore").text(game.yourScore);   
+        crystalValue = parseInt(crystalValue);
+            game.yourScore += crystalValue;
+            $("#yourScore").text(game.yourScore); 
+            // captures wins and losses for the game
+            if (game.yourScore == game.targetScore) {
+                game.wins++;
+                $("#wins").text(game.wins);
+                return false;
+                alert("Winner");
+            } else if (game.yourScore >= game.targetScore) {
+                game.losses++;
+                $("#loss").text(game.losses);
+            }
+            
     });
     
-    if (game.yourScore === game.targetScore) {
-        alert("you Win!");
-        wins++;
-        $("#wins").text(wins);
-        return false;
-    } else if (game.yourScore >= game.targetScore) {
-        alert("Loser!");
-        losses++;
-        $("#loss").text(losses);
-        return false;
-    };
-
     // captures click event from start button
     $("body").on("click", "#start", function() {
     startGame();
